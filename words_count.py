0bb32e141da9bacfb92dc28d1441bef379059062
Naviersh <Naviersh@mail.ru>
2021-10-10 01:44:04 +0500

    Теперь файл считается по строкам

--- words_count.py
+++ words_count.py
@@ -1,17 +1,15 @@
 import re
-import gc
-import numpy
 from collections import Counter
 import time
 
 start_time = time.time()
 f = open('output.txt', 'w')
 cnt = Counter()
-doc = open('100MB.txt', 'r')
-text_string = doc.read().lower()
-opt = re.sub(r'[^\w\s]','', text_string)
-for word in opt.split():
-    cnt[word] += 1
+doc = open('1MB.txt', 'r')
+for line in doc:
+    opt = re.sub(r'[^\w\s]', '',line)
+    for word in line.split():
+        cnt[word] += 1
 f.write(str(cnt))
 f.close()
 file_time = (time.time() - start_time)
